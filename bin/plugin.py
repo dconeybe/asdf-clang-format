30509c706d001c0c4a132bb02af7d969bdecf91b
Denver Coneybeare <dconeybe@google.com>
2025-03-11 04:07:25 +0000

    install more work

--- bin/plugin.py
+++ bin/plugin.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+from collections.abc import Sequence
 import dataclasses
 import logging
-import os
 import sys
 
 import requests
+
 
 def main() -> None:
   arg_parser = argparse.ArgumentParser()
@@ -49,21 +50,51 @@ def main() -> None:
 
 
 def list_all() -> None:
-  versions = get_llvm_versions()
-  print(" ".join(versions))
+  releases = get_llvm_releases()
+  versions_str = " ".join(release.version for release in releases)
+  print(versions_str)
 
 
 def install(version_to_install: str) -> None:
   logging.info("Installing clang-format version %s", version_to_install)
-  versions = get_llvm_versions()
-  print(" ".join(versions))
+  releases = get_llvm_releases()
+  releases_to_install = [release for release in releases if release.version == version_to_install]
+
+  if len(releases_to_install) == 0:
+    print(f"ERROR: version not found: {version_to_install}", file=sys.stderr)
+    sys.exit(1)
+  elif len(releases_to_install) > 1:
+    print(
+        f"ERROR: {len(releases_to_install)} versions found "
+        f"for version {version_to_install}, but expected exactly 1.",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
+  release_to_install = releases_to_install[0]
+  import pprint
+
+  pprint.pprint(releases_to_install)
 
 
-def get_llvm_versions() -> list[str]:
+@dataclasses.dataclass(frozen=True)
+class LlvmReleaseAsset:
+  name: str
+  size: int
+  download_url: str
+
+
+@dataclasses.dataclass(frozen=True)
+class LlvmReleaseInfo:
+  version: str
+  assets: Sequence[LlvmReleaseAsset]
+
+
+def get_llvm_releases() -> list[LlvmReleaseInfo]:
   url = "https://api.github.com/repos/llvm/llvm-project/releases"
   headers = {
-    "Accept": "application/vnd.github+json",
-    "X-GitHub-Api-Version": "2022-11-28",
+      "Accept": "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
   }
   logging.info("Getting releases from %s", url)
 
@@ -71,9 +102,26 @@ def get_llvm_versions() -> list[str]:
   response.raise_for_status()
   releases = response.json()
 
-  release_names = tuple(release["name"] for release in releases)
+  llvm_release_infos: list[LlvmReleaseInfo] = []
+  for release in releases:
+    release_name = release["name"]
+    version = release_name[5:]
 
-  return [release_name[5:] for release_name in release_names]
+    llvm_release_assets: list[LlvmReleaseAsset] = []
+    assets = release.get("assets", [])
+    if assets:
+      for asset in assets:
+        llvm_release_assets.append(
+            LlvmReleaseAsset(
+                name=asset["name"],
+                size=asset["size"],
+                download_url=asset["browser_download_url"],
+            )
+        )
+
+    llvm_release_infos.append(LlvmReleaseInfo(version=version, assets=llvm_release_assets))
+
+  return llvm_release_infos
 
 
 if __name__ == "__main__":
